bfee999593a0b09fe7175beaca9b5a4f27ff3730
znmead <zachary.n.mead@gmail.com>
2026-08-16 12:00:00 -0500

    Optimize query in page.tsx

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import { PortfolioView } from "@/components/portfolio-view"
+
+export default function PortfolioPage() {
+  return (
+    <div className="container mx-auto px-4 py-8 animate-fade-in">
+      <PortfolioView />
+    </div>
+  )
+}
