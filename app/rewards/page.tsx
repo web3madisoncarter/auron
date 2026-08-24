4b5e9b1f3adb81ea22b79bb798efa5169f387e31
ADWilkinson <gm@galleonlabs.io>
2026-08-24 12:00:00 +0300

    Fix bug in page.tsx

---
+++ app/rewards/page.tsx
@@ -0,0 +1,9 @@
+import { RewardsTracker } from "@/components/rewards-tracker"
+
+export default function RewardsPage() {
+  return (
+    <div className="container mx-auto px-4 py-8 animate-fade-in">
+      <RewardsTracker />
+    </div>
+  )
+}
